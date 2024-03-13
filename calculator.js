f64a81e32c952b0ca6f7930975dd46d8416bf6b7
tep-vila <vila.sj.bscs@gmail.com>
2024-03-13 17:53:29 +0800

    equal sign now working. addition only. decimals still not tested.

--- calculator.js
+++ calculator.js
@@ -2,9 +2,10 @@ const display = document.querySelector(".display");
 const numpad = document.querySelector(".numpad");
 const decimalKey = document.querySelector(".numpad.decimal");
 const operationKeys = document.querySelector(".operation-row");
+const equalSign = document.querySelector(".equal-sign");
 
 let value1 = undefined;
-let value2 = 0;
+let value2 = undefined;
 let operator = "";
 let computed = "";
 
@@ -64,4 +65,47 @@ function handleDecimal(e) {
   }
   updateDisplay(e.target.textContent);
   isDecimal = true;
+}
+
+equalSign.addEventListener("mousedown", () => {
+  if (value1 === undefined) {
+    return;
+  } else {
+    value2 = display.textContent;
+    operation();
+    display.textContent = computed;
+    newEquation();
+  }
+});
+
+function operation() {
+  let parsedVal1 = parseFloat(value1);
+  let parsedVal2 = parseFloat(value2);
+  switch (operator) {
+    case "+":
+      add(parsedVal1, parsedVal2);
+      break;
+
+    case "-":
+      subtract(parsedVal1, parsedVal2);
+      break;
+
+    case "*":
+      multiply(parsedVal1, parsedVal2);
+      break;
+
+    case "/":
+      divide(parsedVal1, parsedVal2);
+      break;
+  }
+}
+
+function add(val1, val2) {
+  console.log("occured", val1, val2);
+  computed = val1 + val2;
+}
+
+function newEquation() {
+  value1 = computed;
+  value2 = undefined;
 }
