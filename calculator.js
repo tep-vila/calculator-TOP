33f2d040bbb03b5d31ac133eaed50cab3433cec3
tep-vila <vila.sj.bscs@gmail.com>
2024-03-13 14:40:52 +0800

    decimal now working plus operation keys have event listeners. still need main function.

--- calculator.js
+++ calculator.js
@@ -1,7 +1,14 @@
 const display = document.querySelector(".display");
 const numpad = document.querySelector(".numpad");
+const decimalKey = document.querySelector(".numpad.decimal");
+const operationKeys = document.querySelector(".operation-row");
+
 let value1 = 0;
 let value2 = 0;
+let operator = "";
+let computed = "";
+
+let isDecimal = false;
 
 function updateDisplay(value) {
   if (display.textContent.length >= 14) {
@@ -22,10 +29,26 @@ numpad.addEventListener("mousedown", (e) => {
   if (e.target.textContent === "clear") {
     clear();
     return;
+  } else if (e.target.textContent === ".") {
+    handleDecimal(e);
+    return;
   }
   updateDisplay(e.target.textContent);
 });
 
+operationKeys.addEventListener("mousedown", (e) => {
+  operator = e.target.textContent;
+});
+
 function clear() {
   display.textContent = "0";
+  isDecimal = false;
+}
+
+function handleDecimal(e) {
+  if (isDecimal === true) {
+    return;
+  }
+  updateDisplay(e.target.textContent);
+  isDecimal = true;
 }
